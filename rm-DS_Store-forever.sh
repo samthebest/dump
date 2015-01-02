e484c3b2f0f1d309b395097b5893ae874cceeca4
samthebest <samthesavage@gmail.com>
2015-01-02 23:31:56 +0000

    Update rm-DS_Store-forever.sh

--- rm-DS_Store-forever.sh
+++ rm-DS_Store-forever.sh
@@ -1,5 +1,1 @@
-echo "while true; do find / -name .DS_Store -exec echo \"{}\" \; ; sleep 2; done" > ~/.rm-DS_Store-abomination.sh
-chmod +x ~/.rm-DS_Store-abomination.sh
-
-
-screen -ls | grep rm-DS_Store-abomination >/dev/null || screen -S rm-DS_Store-abomination -d -m ~/.rm-DS_Store-abomination.sh
+echo "while true; do find / -name .DS_Store -exec echo \"{}\" \; ; sleep 2; done" > ~/.rm-DS_Store-abomination.sh && chmod +x ~/.rm-DS_Store-abomination.sh && echo "screen -ls | grep rm-DS_Store-abomination >/dev/null || screen -S rm-DS_Store-abomination -d -m ~/.rm-DS_Store-abomination.sh" >> ~/.bash_profile
