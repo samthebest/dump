f44654365800d3c4e9df1e8bd38480a11f6479e3
samthebest <samthesavage@gmail.com>
2015-01-18 12:31:24 +0000

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# sbt
+echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-get update
+sudo apt-get install sbt
+
+# git
+sudo apt-get install git
+
+# mkdir src
+mkdir src
