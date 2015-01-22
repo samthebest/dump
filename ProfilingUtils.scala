1a4be5894ff035631571d22e9193a97ad3167639
samthebest <samthesavage@gmail.com>
2015-01-22 18:50:33 +0000

    Create ProfilingUtils.scala

---
+++ ProfilingUtils.scala
@@ -0,0 +1,19 @@
+object ProfilingUtils {
+  def time(f: => Unit): Double = {
+    val now = System.nanoTime
+    f
+    val took = System.nanoTime - now
+    println("Took " + took / (1000.0 * 1000.0) + " milli seconds")
+    took
+  }
+
+  def doMany(n: Int, f: => Unit): Unit =
+    (1 to n).foreach(i => f)
+
+  def timeMany(n: Int, f: => Unit): Double = time(doMany(n, f))
+
+  def example: Double =
+    timeMany(100, {
+      println("Hello world, I'm being profiled")
+    })
+}
