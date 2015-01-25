fe7fbb4e5df1920135fba79b643539cf56eaf367
samthebest <samthesavage@gmail.com>
2015-01-25 17:10:07 +0000

    Update launch.sh

--- test/launch.sh
+++ test/launch.sh
@@ -21,16 +21,16 @@ if [ "$2" = "prod" ]
 then
     interface="0.0.0.0"
     port=80
-    use-sudo="sudo "
+    use_sudo="sudo "
     shift
 else
     interface="localhost"
     port=8080
-    use-sudo=""
+    use_sudo=""
 fi
     
-$use-sudo java -cp target/scala-2.11/openmaths-assembly-0.1.0.jar io.openmaths.Main $interface $port $*  
-    
+screen -dm $use_sudo java -cp target/scala-2.11/openmaths-assembly-0.1.0.jar io.openmaths.Main $interface $port $*  
+
 
 
 
