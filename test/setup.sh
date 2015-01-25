325ca1e866c40f69cbff92bc31112c71f3557044
samthebest <samthesavage@gmail.com>
2015-01-25 16:38:44 +0000

    Update setup.sh

--- test/setup.sh
+++ test/setup.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+sudo apt-get install git
+mkdir src
+cd src
+git clone https://github.com/OpenMaths/open-maths.git
+git clone https://github.com/OpenMaths/open-maths-content.git
+# Just in case
+mkdir open-maths-content/json
+mkdir open-maths-content/latex
+
+# Will pull from a repo in future
+mkdir user-db/actions
+mkdir user-db/users
+
+
