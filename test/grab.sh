e87a0a2195208e742b9cdb288dae0f76b8cfb0e8
samthebest <samthesavage@gmail.com>
2015-01-25 21:25:56 +0000

    Create grab.sh

---
+++ test/grab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/samthebest/dump/master/test/setup.sh && chmod +x setup.sh && ./setup.sh
+
+wget https://raw.githubusercontent.com/samthebest/dump/master/test/launch.sh && chmod +x launch.sh && ./launch.sh prod
