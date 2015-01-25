4d2f30a6607f73b2687b153e4c6286aee2c28896
samthebest <samthesavage@gmail.com>
2015-01-25 21:37:17 +0000

    Update launch.sh

--- test/launch.sh
+++ test/launch.sh
@@ -32,7 +32,7 @@ else
     use_sudo=""
 fi
     
-screen -dm $use_sudo java -cp target/scala-2.11/openmaths-assembly-0.1.0.jar io.openmaths.Main $interface $port $* 2>&1 | tee -a ~/open-maths.log
+screen -dm $use_sudo java -cp target/scala-2.11/openmaths-assembly-0.1.0.jar io.openmaths.Main $interface $port $* 2>&1 | tee -a open-maths.log
 
 
 
