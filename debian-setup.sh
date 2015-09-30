c48ab455d5e2208880fa8943bdb02d7d28caa914
samthebest <samthesavage@gmail.com>
2015-09-30 12:18:08 +0100

    Added hipchat

--- debian-setup.sh
+++ debian-setup.sh
@@ -67,6 +67,14 @@ ssh-add ~/.ssh/id_rsa
 xclip -sel clip < ~/.ssh/id_rsa.pub
 # MANUAL STEP - Paste into github https://github.com/settings/ssh
 
+# Hipchat
+sudo su 
+echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
+wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+apt-get update
+apt-get install hipchat
+exit
+
 # Clone my open-source repos
 cd ~/src
 #git clone git@github.com:samthebest/sceval.git
