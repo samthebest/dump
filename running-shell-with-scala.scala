32779bb0a49c705e63a7d0c62e42ec39e5ddae7d
samthebest <samthesavage@gmail.com>
2015-10-07 16:53:01 +0100

    Update running-shell-with-scala.scala

--- running-shell-with-scala.scala
+++ running-shell-with-scala.scala
@@ -2,3 +2,10 @@
 // Doesn't work in scala repl for some reason cos of weird implicit not working
 
 def executeAndDiscardStdErr(s: String): String = s !! ProcessLogger(_ => ())
+
+def executeString(s: String): (String, String, Int) = {
+    var stderr = ""
+    var stdout = ""
+    val exitCode = s ! ProcessLogger(stdout += _, stderr += _)
+    (stdout, stderr, exitCode)
+  }
