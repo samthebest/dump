f7ab554770af0184ee2071dffc7bbfd4b39bbff3
samthebest <samthesavage@gmail.com>
2015-10-13 15:31:49 +0100

    Update running-shell-with-scala.scala

--- running-shell-with-scala.scala
+++ running-shell-with-scala.scala
@@ -9,3 +9,22 @@ def executeString(s: String): (String, String, Int) = {
     val exitCode = s ! ProcessLogger(stdout += _, stderr += _)
     (stdout, stderr, exitCode)
   }
+
+case class BashArgsParser(input: ParserInput) extends Parser {
+    def commandLine: Rule1[Seq[String]] = rule((word ~ zeroOrMore(" " ~ arg)) ~> ((_: String) +: (_: Seq[String])))
+    def arg: Rule1[String] = rule(quotedWords | word)
+    def word: Rule1[String] = rule(capture(oneOrMore(notSpace)))
+    def quotedWords: Rule1[String] = rule("'" ~ capture(oneOrMore(noneOf("'"))) ~ "'")
+    def notSpace: Rule0 = rule(noneOf(" "))
+  }
+
+  def bashStringToSeq(s: String): Seq[String] = BashArgsParser(s).commandLine.run().get
+
+  def executeString(s: String): (String, String, Int) = executeSeq(bashStringToSeq(s))
+
+  def executeSeq(s: Seq[String]): (String, String, Int) = {
+    var stderr = ""
+    var stdout = ""
+    val exitCode = s ! ProcessLogger(stdout += "\n" + _, stderr += "\n" + _)
+    (stdout, stderr, exitCode)
+  }
