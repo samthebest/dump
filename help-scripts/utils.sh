126bb2ceedbb36b807b5084bc24eb7740fd3b09c
samthebest <samthesavage@gmail.com>
2015-10-21 12:37:38 +0100

    Update utils.sh

--- help-scripts/utils.sh
+++ help-scripts/utils.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+version_key="version := \""
+
+# TODO DRY these two
 function get-version {
-    version_key="version := \""
     version_key_len=`echo $version_key | wc -c`
     cat build.sbt | grep "$version_key" | cut -c ${version_key_len}- | rev | cut -c 2- | rev
 }
