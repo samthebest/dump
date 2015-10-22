f4008c4617966a4ce583edfe7827e23bbd596235
samthebest <samthesavage@gmail.com>
2015-10-22 11:42:07 +0100

    Create publish-to-maven-repo.sh

---
+++ help-scripts/publish-to-maven-repo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+source ./scripts/utils.sh
+
+version=`get-version`
+
+maven_creds=/var/lib/jenkins/.ivy2/.credentials
+
+user=`get-value-from-file "user=" $maven_creds`
+password=`get-value-from-file "password=" $maven_creds`
+
+echo "INFO: Publishing to maven repo!"
+curl -v \
+    -F "r=releases" \
+    -F "g=com.mendeley.altmetrics" \
+    -F "a=altmetrics" \
+    -F "v=${version}" \
+    -F "p=jar" \
+    -F "file=@./target/scala-2.10/altmetrics-assembly-${version}.jar" \
+    -u "${user}:${password}" \
+    http://packages.dev.pomboo.net:80/nexus/content/repositories/releases/ 2>&1 | tee curl-publish.log
+
+unauthorized="401 Unauthorized"
+status=`cat curl-publish.log | grep -o "${unauthorized}"`
+
+if [ "${status}" = "${unauthorized}" ]; then
+	echo "ERROR: not authorized to publish to maven"
+	exit 1
+fi
+
+echo "INFO: Probably published to maven OK"
+
+rm curl-publish.log
