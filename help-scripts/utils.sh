a97f8af0925621fad0f0b5fe87d2b9dbb74229df
samthebest <samthesavage@gmail.com>
2015-10-22 11:39:30 +0100

    Update utils.sh

--- help-scripts/utils.sh
+++ help-scripts/utils.sh
@@ -7,3 +7,10 @@ function get-version {
     version_key_len=`echo $version_key | wc -c`
     cat build.sbt | grep "$version_key" | cut -c ${version_key_len}- | rev | cut -c 2- | rev
 }
+
+function get-value-from-file {
+	key="${1}"
+	file="${2}"
+    key_len=`echo ${key} | wc -c`
+    cat "${file}" | grep "${key}" | cut -c ${key_len}-
+}
