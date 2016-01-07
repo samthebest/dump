defc380f31177ca05c6865255ea8bcaa529f8579
samthebest <samthesavage@gmail.com>
2016-01-07 17:19:37 +0000

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -30,6 +30,7 @@ sudo apt-get install google-chrome-stable
 sudo apt-get install git
 
 # Java
+RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
