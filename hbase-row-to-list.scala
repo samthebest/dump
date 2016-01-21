0de12ab113421cf77a1213ad3d4ea3963314bc5b
samthebest <samthesavage@gmail.com>
2016-01-21 16:55:30 +0000

    Create hbase-row-to-list.scala

---
+++ hbase-row-to-list.scala
@@ -0,0 +1,13 @@
+def hbaseRowToStringList(result: Result): List[(String, String, String, Long)] = {
+    for {
+      columnFamilyMap <- result.getMap.entrySet().toSet.toList
+      entryVersion <- columnFamilyMap.getValue().entrySet().toSet.toList
+      entry <- entryVersion.getValue().entrySet().toSet.toList
+    } yield {
+      val row: String = Bytes.toString(result.getRow())
+      val column: String = Bytes.toString(entryVersion.getKey())
+      val value: String = Bytes.toString(entry.getValue())
+      val ts: Long = entry.getKey()
+      (row, column, value, ts)
+    }
+  }
