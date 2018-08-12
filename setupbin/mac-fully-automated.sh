21a44c44b12a1e40e3a6f03336a32a09d445a9f5
samthebest <samthebest@users.noreply.github.com>
2018-08-12 09:58:04 -0700

    Create mac-fully-automated.sh

---
+++ setupbin/mac-fully-automated.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+
+# Install sublime (best text editor ever!)
+brew cask install sublime-text
+
+# Install java
+brew cask install java
+
+# Install scala
+brew install scala
+
+# Install sbt
+brew install sbt
+
+# Install Intellij (Community edition) (doesn't seem to work anymore)
+# brew cask install intellij-idea-ce
+
+# ifstat
+brew install ifstat
+
+# firefox
+brew cask install firefox
+
+# Install GNU style bash commands (gives gdate and such and such)
+brew install coreutils
+
+# Hipchat
+brew cask install hipchat
+
+# DEPRECATED use oneflow
+# git-flow
+# brew install git-flow
+
+# git auto completion
+brew install git bash-completion
+
+# Skip annoying "yes" for cloning git repos
+git config --global http.sslVerify false
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+# spotify for concentration
+brew cask install spotify
+
+# open office
+brew cask install openoffice
+
+# wget
+brew install wget
+
+# s3cmd
+brew install s3cmd
+
+# Source tree
+brew cask install sourcetree
+
+# really cool json tool
+brew install jq
+
+# install pip
+brew install python
+
+# install aws cli
+pip install awscli
+
+# vagrant - a orchestration for virtual box
+brew cask install vagrant
+
+# VLC
+brew cask install vlc
+
+# Virtual box
+brew cask install virtualbox
+
