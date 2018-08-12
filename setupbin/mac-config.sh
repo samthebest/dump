967c04ee34caee1db53520953160cc996ed05f2a
samthebest <samthebest@users.noreply.github.com>
2018-08-12 18:56:00 +0100

    Update mac-config.sh

--- setupbin/mac-config.sh
+++ setupbin/mac-config.sh
@@ -12,3 +12,8 @@ defaults write .GlobalPreferences com.apple.trackpad.scaling -1
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+# Change dock size to be basically invisible
+defaults write com.apple.dock tilesize -int 1; killall Dock
+
+# Track direction, and turning off stupid gestures and stuff is all manual.
+
