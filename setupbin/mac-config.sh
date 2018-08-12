d65c57dd28f8c38330054c3486294042fe1c4638
samthebest <samthebest@users.noreply.github.com>
2018-08-12 18:54:12 +0100

    Create mac-config.sh

---
+++ setupbin/mac-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Show Battery %
+defaults write com.apple.menuextra.battery ShowPercent YES
+killall SystemUIServer
+
+# Turn off annoying stuff
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
+defaults write .GlobalPreferences com.apple.trackpad.scaling -1
+
+# Fix key repeat settings
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
