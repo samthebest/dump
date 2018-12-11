8539c1afae61c627dec362eb9b881e4099a4df95
samthebest <samthebest@users.noreply.github.com>
2018-12-11 11:53:57 +0000

    Update mac-config.sh

--- setupbin/mac-config.sh
+++ setupbin/mac-config.sh
@@ -13,7 +13,11 @@ defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # Change dock size to be basically invisible
-defaults write com.apple.dock tilesize -int 1; killall Dock
+defaults write com.apple.dock tilesize -int 1
+defaults write com.apple.dock largesize -float 512
+killall Dock
+
+# Have to enable dock zoom manually
 
 # Track direction, and turning off stupid gestures and stuff is all manual.
 
