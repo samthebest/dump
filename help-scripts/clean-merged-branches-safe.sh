ea4b3afdddaefba3222f7e73457e217f56293ae1
samthebest <samthebest@users.noreply.github.com>
2019-12-05 11:10:10 +0000

    Update clean-merged-branches-safe.sh

--- help-scripts/clean-merged-branches-safe.sh
+++ help-scripts/clean-merged-branches-safe.sh
@@ -14,6 +14,8 @@ function track-all {
 track-all
 
 for branch in `git branch --merged master | grep -v master`; do
+  git checkout $branch
+  git pull origin $branch
   git merge master
   git diff master --stat
   git push origin $branch
@@ -21,6 +23,7 @@ for branch in `git branch --merged master | grep -v master`; do
   echo "Differences: \"$differences\""
   if [ "$differences" = "0" ]; then
     echo "Deleting local branch: $branch"
+    git checkout master
     git branch -d $branch || break
     echo "Deleting remote branch: $branch"
     git push --delete origin $branch || break
